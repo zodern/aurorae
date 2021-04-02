9c7deb6431bda02b6ca4b6c4bf978e8fe3d94d66
zodern <zodern@users.noreply.github.com>
2021-04-01 21:17:13 -0500

    Move build plugin to separate package

--- package.js
+++ package.js
@@ -1,19 +1,14 @@
 Package.describe({
   name: 'zodern:aurorae',
   description: 'Storybook for Meteor',
-  devOnly: true,
 });
 
 Package.onUse(api => {
   api.versionsFrom('2.0')
   api.use('svelte:compiler@3.31.2');
   api.use('ecmascript');
-  api.mainModule('./client.js', 'client');
+  api.use('dynamic-import');
+  api.mainModule('./main.js', 'client');
   api.use('isobuild:compiler-plugin@1.0.0');
-});
-
-Package.registerBuildPlugin({
-  name: 'compile-stories',
-  use: ['babel-compiler', 'react-fast-refresh'],
-  sources: ['build-plugin.js']
+  api.imply('zodern:aurorae-compiler');
 });
