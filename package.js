f744c509099de998c74ac940262e5fe8356557d7
zodern <zodern@users.noreply.github.com>
2021-04-21 15:46:21 -0500

    Update dependencies

--- package.js
+++ package.js
@@ -10,9 +10,9 @@ Package.onUse(api => {
   api.versionsFrom('2.0')
   api.use('ecmascript');
   api.use('dynamic-import');
-  api.use('zodern:melte@1.4.2');
+  api.use('zodern:melte@1.4.4');
 
-  api.imply('zodern:aurorae-compiler@0.1.0');
+  api.imply('zodern:aurorae-compiler@0.1.2');
 
   api.mainModule('./main.js', 'client');
   api.mainModule('./server.js', 'server');
